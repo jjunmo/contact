7db5db36731e11a44e289c9982eb834f643d3199
jjunmo <yymeyy2002@gmail.com>
2022-10-11 18:06:19 +0900

    레이아웃 설정 당근

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,42 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home:Scaffold(
         appBar: AppBar(
-          title: Text('앱임'),
+         title: Text("금호동 3가"), actions: [Icon(Icons.star),Icon(Icons.star),Icon(Icons.star),],
           centerTitle: false,
         ),
-        body: Container(
-          width: 200,height: 200,
-          decoration: BoxDecoration(
-              border: Border.all(color: Colors.black)
+        body:Container(
+          height: 150,
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Flexible(child: Container(
+                child: Image.asset('assets/kakaoImage.jpeg'),
+              ),
+                flex: 3,
+              ),
+              Flexible(child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('캐논 DSLR 100D',style: TextStyle(fontSize: 30),),
+                    Text('위치'),
+                    Text('가격'),
+                    Row(
+
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Icon(Icons.favorite),
+                        Text('4'),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+                flex: 7,
+              ),
+            ],
           ),
+
         ),
         ),
     );
